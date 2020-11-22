590bd1913b028303724dfb67b768f0f5b2eac104
kevin-shannon <kevers429@gmail.com>
2020-11-22 05:20:34 -0700

    login and cart

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const path = require('path');
 
 // Init App
@@ -10,6 +11,16 @@ var db = require('./queries.js')
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use((req, res, next) => {
+  res.locals.username = req.query.username;
+  res.locals.path = req.path;
+  res.locals.loginInfo = db.userify(req.query);
+  next();
+})
+
 // Home Route
 app.get('/', (req, res) => {
   res.render('index', {
@@ -18,32 +29,100 @@ app.get('/', (req, res) => {
 })
 
 // Add Routes
-app.get('/books', function(req, res) {
-  db.getAllBooks.then((data) => {
+app.get('/login', (req, res) => {
+  const {username, password} = req.query;
+
+  if (!username) {
+    res.render('login', {
+      success: false
+    })
+  }
+  else {
+    db.checkValidUser(username, password).then((user) => {
+      res.render('login', {
+        success: true
+      })
+    })
+    .catch(() => {
+      res.redirect(req.path);
+    })
+  }
+})
+
+app.get('/books', (req, res) => {
+  db.getAllBooks().then((books) => {
     res.render('books', {
-      title:'Books',
-      books: data
+      title: 'Books',
+      books
     })
   });
 })
 
-app.get('/titles', function(req, res) {
-  db.getAvailableTitles.then((data) => {
+app.get('/titles', (req, res) => {
+  db.getAvailableTitles().then((titles) => {
     res.render('titles', {
-      title:'Titles',
-      titles: data
+      title: 'Titles',
+      titles
     })
   });
 })
 
-app.get('/vendors', function(req, res) {
-  db.getAllVendors.then((data) => {
+app.get('/vendors', (req, res) => {
+  db.getAllVendors().then((vendors) => {
     res.render('vendors', {
-      title:'Vendors',
-      vendors: data
+      title: 'Vendors',
+      vendors
     })
   });
 })
+
+app.get('/titles/:isbn', (req, res) => {
+  let t = db.getTitle(req.params.isbn)
+  let s = db.getSellers(req.params.isbn)
+
+  Promise.all([t,s]).then(([title,sellers]) => {
+    res.render('title', {
+      title,
+      sellers
+    })
+  })
+})
+
+app.get('/vendors/:vendor_id', (req, res) => {
+  db.getVendor(req.params.vendor_id).then((vendor) => {
+    res.render('vendor', {
+      vendor
+    })
+  });
+})
+
+app.get('/cart', (req, res) => {
+  let c = db.getCart(req.query.username)
+  let n = db.getNumItems(req.query.username)
+  let p = db.getTotalPrice(req.query.username)
+
+  Promise.all([c,n,p]).then(([cart, numItems, totalPrice]) => {
+    res.render('cart', {
+      cart,
+      numItems,
+      totalPrice
+    })
+  })
+})
+
+// Post Requests
+app.post('/cart/add', (req, res) => {
+  db.addToCart(req.body.book_id, req.body.username).then(() => {
+    res.redirect('/cart' + req.body.loginInfo);
+  })
+})
+
+app.post('/cart/remove', (req, res) => {
+  db.removeFromCart(req.body.book_id, req.body.username).then(() => {
+    res.redirect('/cart' + req.body.loginInfo);
+  })
+})
+
 
 // Start Server
 app.listen(port, () => {
--- queries.js
+++ queries.js
@@ -3,28 +3,66 @@ var pgp = require('pg-promise')({});
 const cn = 'postgres://postgres:412@localhost:5432/bookstore';
 const db = pgp(cn);
 
+const userify = ({username, password}) => (username && password) ? `?username=${username}&password=${password}` : '';
+
 // Query functions
 
-const getAllBooks = db.any('SELECT book_id, title.name, book.isbn, vendor.name AS vendor_name, author.name AS author_name, admin.username \
+const getAllBooks = () => db.any('SELECT book_id, title.name, book.isbn, vendor.name AS vendor_name, author.name AS author_name, admin.username \
   FROM book, title, vendor, admin, author, written_by \
-  WHERE book.ISBN = title.ISBN AND book.admin_id = admin.admin_id AND book.vendor_id = vendor.vendor_id AND title.isbn = written_by.isbn AND written_by.author_id = author.author_id;')
-  .then((data) => {
-    return data;
-  })
+  WHERE book.ISBN = title.ISBN AND book.admin_id = admin.admin_id AND book.vendor_id = vendor.vendor_id AND title.isbn = written_by.isbn AND written_by.author_id = author.author_id;');
 
-const getAvailableTitles = db.any('SELECT name, rating, price FROM book, title WHERE book.ISBN = title.ISBN AND customer_id is null')
-  .then((data) => {
-    return data;
-  })
+const getAvailableTitles = () =>db.any('SELECT name, rating, price, title.isbn FROM book, title WHERE book.ISBN = title.ISBN AND customer_id is null');
 
-const getAllVendors = db.any('SELECT * FROM Vendor')
-  .then((data) => {
-    console.log(data);
-    return data;
-  })
+const getAllVendors = () => db.any('SELECT * FROM Vendor');
+
+const getCart = (username) => db.any('SELECT book_id, title.name, price, book.vendor_id, vendor.name AS vendor \
+  FROM book, vendor, title, customer \
+  WHERE book.customer_id = customer.customer_id AND customer.username = $1 AND book.isbn = title.isbn AND book.vendor_id = vendor.vendor_id;', username)
+
+const getNumItems = (username) => db.one('SELECT COUNT(price) \
+  FROM book, vendor, title, customer \
+  WHERE book.customer_id = customer.customer_id AND customer.username = $1 AND book.isbn = title.isbn AND book.vendor_id = vendor.vendor_id;', username);
+
+const getTotalPrice = (username) => db.one('SELECT SUM(price) \
+FROM book, vendor, title, customer \
+WHERE book.customer_id = customer.customer_id AND customer.username = $1 AND book.isbn = title.isbn AND book.vendor_id = vendor.vendor_id;', username);
+
+const getTitle = (isbn) => db.one('SELECT * FROM title WHERE isbn = $1;', isbn)
+
+const getSellers = (isbn) =>
+  db.any('SELECT book_id, condition, admin.username AS published_by, admin.admin_id, vendor.name AS vendor, vendor.vendor_id \
+    FROM book, admin, vendor \
+    WHERE isbn = $1 and customer_id is null AND book.vendor_id = vendor.vendor_id AND book.admin_id = admin.admin_id;', isbn)
+
+const getVendor = (vendor_id) => db.one('SELECT * FROM vendor WHERE vendor_id = $1', vendor_id);
+
+const checkValidUser = (username, password) => db.one('SELECT * FROM customer WHERE username = $1 AND password = $2', [username, password]);
+
+// Modifiers
+
+const addToCart = (book_id, username) => db.any('UPDATE book \
+  SET customer_id = customer.customer_id \
+  FROM customer \
+  WHERE book.book_id = $1 AND customer.username = $2;', [book_id, username]);
+
+const removeFromCart = (book_id, username) => db.any('UPDATE book \
+  SET customer_id = NULL \
+  FROM customer \
+  WHERE book.book_id = $1 AND customer.username = $2;', [book_id, username]);
 
 // Exports
-
-exports.getAllBooks = getAllBooks;
-exports.getAvailableTitles = getAvailableTitles;
-exports.getAllVendors = getAllVendors;
+module.exports = {
+  userify,
+  getAllBooks,
+  getAvailableTitles,
+  getAllVendors,
+  getCart,
+  getNumItems,
+  getTotalPrice,
+  getTitle,
+  getSellers,
+  getVendor,
+  checkValidUser,
+  addToCart,
+  removeFromCart
+}

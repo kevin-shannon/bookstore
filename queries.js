4dd354e80e469de4b8de09bead4df7f36585e34c
kevin-shannon <kevers429@gmail.com>
2020-11-23 09:24:34 -0700

    added authors and genres to title

--- queries.js
+++ queries.js
@@ -44,7 +44,17 @@ const getTotalPrice = (customer_id) => db.one('SELECT SUM(price) \
 FROM book, vendor, title, customer \
 WHERE book.customer_id = customer.customer_id AND customer.customer_id = $1 AND book.isbn = title.isbn AND book.vendor_id = vendor.vendor_id;', customer_id);
 
-const getTitle = (isbn) => db.one('SELECT * FROM title WHERE isbn = $1;', isbn)
+const getTitle = (isbn) => db.one("SELECT name, rating, price, published_date, title_agg.isbn, genre_list, author_list \
+  FROM (SELECT name, rating, price, published_date, title_agg.isbn, genre_list \
+        FROM (SELECT isbn, STRING_AGG(type, ', ') AS genre_list FROM categorized_by GROUP BY isbn) AS genre_agg, \
+             (SELECT name, rating, MIN(price) AS price, published_date, title.isbn FROM title LEFT JOIN book ON title.isbn = book.isbn GROUP BY title.isbn) AS title_agg \
+        WHERE genre_agg.isbn = title_agg.isbn) AS title_agg \
+  LEFT JOIN (SELECT isbn, STRING_AGG(name::character varying, ', ') author_list \
+             FROM (SELECT isbn, name \
+                   FROM written_by, author \
+                   WHERE written_by.author_id = author.author_id) AS by_name GROUP BY isbn) AS written_by_agg \
+  ON title_agg.isbn = written_by_agg.isbn \
+  WHERE title_agg.isbn = $1;", isbn);
 
 const getSellers = (isbn) =>
   db.any('SELECT book_id, condition, admin.username AS published_by, admin.admin_id, vendor.name AS vendor, vendor.vendor_id \
